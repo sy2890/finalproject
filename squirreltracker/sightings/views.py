24603cbb082d52500215cbe35f1d952316f6953b
Siyuan Ying <sy2890@columbia.edu>
2019-12-07 02:14:39 +0000

    Edit the sightings view

--- squirreltracker/sightings/views.py
+++ squirreltracker/sightings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Squirrel
+def sightings(request):
+    if request.method == "GET":
+        lst = Squirrel.objects.all()
+        return render(request,'sightings/sightingsList.html',{'lst':lst})
 # Create your views here.
