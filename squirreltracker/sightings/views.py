c35cbe357831b882b1884157e0f60034c3f4ae11
Guinan Xiang <gx2143@columbia.edu>
2019-12-07 23:40:43 +0000

    modify sightings.html

--- squirreltracker/sightings/views.py
+++ squirreltracker/sightings/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render,redirect
-from django.urls import reverse, reverse_lazy
-from django.views.generic.edit import CreateView
 from .forms import SightingsForm
 from .models import Squirrel
 
 def sightings(request):
     if request.method == "GET":
-        lst = Squirrel.objects.all()
-        return render(request,'sightings/sightingsList.html',{'lst':lst})
+        sightings = Squirrel.objects.all()
+        return render(request,'sightings/sightings.html',{'sightings':sightings,})
 
 def add(request):
     if request.method == 'POST':
@@ -17,7 +15,7 @@ def add(request):
             return redirect("/sightings/add")
     else:
         form = SightingsForm()
-    return render(request,'sightings/addSightings.html',{'form':form})
+    return render(request,'sightings/addSightings.html',{'form':form,})
 
 def update(request):
     if request.method == 'POST':
