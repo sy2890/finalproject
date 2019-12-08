c377432998754399d14e131dada0f65b51cd4494
Guinan Xiang <gx2143@columbia.edu>
2019-12-08 05:19:42 +0000

    modify files about map app

--- squirreltracker/map/urls.py
+++ squirreltracker/map/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-form . import views
+from . import views
 
 urlpatterns = [
         path('',views.map,name='map'),
--- squirreltracker/map/views.py
+++ squirreltracker/map/views.py
@@ -3,8 +3,8 @@ from django.http import Http404
 from sightings.models import Squirrel
 
 def map(request):
-    squirrels = Squirrel.object.all()[:100]
-    return render(request,'map/map.html',{'squirrels' = squirrels})
+    squirrels = Squirrel.objects.all()[:100]
+    return render(request,'map/map.html',{'squirrels': squirrels})
 
 
 # Create your views here.
