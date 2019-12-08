ae5017b7c189aa99b9920278be8fe3068cbd20e5
Guinan Xiang <gx2143@columbia.edu>
2019-12-08 04:49:52 +0000

    modify views.py for map

--- squirreltracker/map/views.py
+++ squirreltracker/map/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import Http404
+from sightings.models import Squirrel
+
+def map(request):
+    squirrels = Squirrel.object.all()[:100]
+    return render(request,'map/map.html',{'squirrels' = squirrels})
+
 
 # Create your views here.
