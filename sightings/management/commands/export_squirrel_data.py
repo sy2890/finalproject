98dda3836ed194b5a175db406105ec1ed92ba65a
Guinan Xiang <gx2143@columbia.edu>
2019-12-08 21:37:54 +0000

    modify export file

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -4,7 +4,7 @@ import sys
 import csv
 class Command(BaseCommand):
     help = 'Export data to a csv file'
-
+    
     def add_arguments(self, parser):
         parser.add_argument('file_path', help='filepath name')
 
@@ -13,5 +13,7 @@ class Command(BaseCommand):
         column_names = [f.name for f in meta.fields]
         file_path = options['file_path']
         with open(file_path,'w') as csvfile:
-            writer = csv.writer(csvfile)                                                    writer.writerow(column_names)
-            for instance in Squirrel.objects.all():                                             writer.writerow([getattr(instance, field) for field in column_names])
+            writer = csv.writer(csvfile)                                                    
+            writer.writerow(column_names)
+            for instance in Squirrel.objects.all():                                            
+                writer.writerow([getattr(instance, field) for field in column_names])
