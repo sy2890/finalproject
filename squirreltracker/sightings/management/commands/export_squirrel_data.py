245ffb1728650c60d069d98baad5294ce7cd7c16
Guinan Xiang <gx2143@columbia.edu>
2019-12-08 02:15:16 +0000

    add file to export data

---
+++ squirreltracker/sightings/management/commands/export_squirrel_data.py
@@ -0,0 +1,17 @@
+from sightings.models import Squirrel
+from django.http import HttpResponse
+import sys
+import csv
+class Command(BaseCommand):
+    help = 'Export data to a csv file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', help='filepath name')
+
+    def handle(self, *args, **options):
+        meta = Squirrel._meta
+        column_names = [f.name for f in meta.fields]
+        file_path = options['file_path']
+        with open(file_path,'w') as csvfile:
+            writer = csv.writer(csvfile)                                                    writer.writerow(column_names)
+            for instance in Squirrel.objects.all():                                             writer.writerow([getattr(instance, field) for field in column_names])
